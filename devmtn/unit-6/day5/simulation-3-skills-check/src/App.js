47b01f63e4d0958b31fab0e3f61ba8c4d82441fe
MrNsmith <nicholas88smith@gmail.com>
2020-07-12 15:30:14 -0600

    routeing done

--- devmtn/unit-6/day5/simulation-3-skills-check/src/App.js
+++ devmtn/unit-6/day5/simulation-3-skills-check/src/App.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
-import Auth from './Components/Auth/Auth';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Form from './Components/Form/Form';
-import Post from './Components/Post/Post';
+import routes from './routes'
 import Nav from './Components/Nav/Nav';
 
 import './App.css';
@@ -10,13 +7,12 @@ import './App.css';
 
 class App extends Component {
     render(){
+        
         return(
             <div className='App'>
               <Nav/>
-              <Auth/>
-              <Dashboard/>
-              <Form/>
-              <Post/>
+              {routes}
+              
             </div>
         )
     }
--- devmtn/unit-6/day5/simulation-3-skills-check/src/Components/Nav/Nav.js
+++ devmtn/unit-6/day5/simulation-3-skills-check/src/Components/Nav/Nav.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import {withRouter, Link} from 'react-router-dom'
 
 class Nav extends Component {
     render(){
         return(
-            <div>Nav</div>
+            <div>
+                {this.props.location.pathname !=='/'
+                ?(<nav>
+
+                <h1>Nav</h1>
+                    <Link to = '/dashboard'>Home</Link>
+                    <Link to = '/new'>New Post</Link>
+                    <Link to= '/'>Logout</Link>
+
+                </nav>)
+                : null }
+                
+                
+            </div>
         )
     }
 }
-export default Nav;
+export default withRouter(Nav);
